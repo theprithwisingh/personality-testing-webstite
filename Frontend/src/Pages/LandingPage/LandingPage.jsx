79a6808141854ecd33ae951612efce632a4f56c9
theprithwisingh <prithwisingh77@gmail.com>
2024-10-07 23:56:59 +0530

    update

--- Frontend/src/Pages/LandingPage/LandingPage.jsx
+++ Frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,19 +1,20 @@
+
 import React from 'react';
-import './LandingPage.css'
-import backGroundImg from '../../assets/bg-1.jpg'
+import './LandingPage.css';
 
 const LandingPage = () => {
   return (
-
-    <div className="image-container">
-      <div className="text-content">
-        <h1>"Discover Your True Personality"</h1>
-        <p>Unlock the secrets to your unique strengths, behaviors, and preferences with our in-depth personality test.</p>
-        <button className="test-button">Take the Free Test</button>
-        <p> Unlock the best version of yourself. Take the test in under 10 minutes!</p>
-      </div>
+    <div className="landing-page">
+          <div className='first'>
+          <h1>Discover</h1>
+          <h1>Your Strengths,</h1>
+          <h1>Boost Your Confidence.</h1>
+          </div>
+          <div className='second'>
+          <p>Unlock the secrets to your unique strengths, behaviors, and preferences with our in-depth personality test.</p>
+          <button>Take the Free Test</button>
+          </div>
     </div>
-
   );
 };
 
